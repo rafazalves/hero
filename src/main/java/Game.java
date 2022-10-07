0f8a94f5d9162e3f920f63a1fc07554d88a9f50d
unknown <rafael.alves238@gmail.com>
2022-10-07 23:05:51 +0100

    Hero Setup

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -24,11 +24,53 @@ public class Game {
             e.printStackTrace();
         }
 
+        arena = new Arena(40, 20);
     }
 
     private void draw() throws IOException{
         screen.clear();
+        arena.draw(screen.newTextGraphics());
         screen.refresh();
     }
 
+    private void processKey(com.googlecode.lanterna.input.KeyStroke key){
+        System.out.println(key);
+        switch (key.getKeyType()) {
+            case ArrowUp    -> arena.moveHero(arena.moveUp());
+            case ArrowDown  -> arena.moveHero(arena.moveDown());
+            case ArrowLeft  -> arena.moveHero(arena.moveLeft());
+            case ArrowRight -> arena.moveHero(arena.moveRight());
+        }
+    }
+
+    public void run() {
+        try {
+            while(true) {
+                draw();
+                com.googlecode.lanterna.input.KeyStroke key = screen.readInput();
+                processKey(key);
+                if(arena.verifyMonsterCollisions()){
+                    screen.close();
+                    break;
+                }
+
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == ('q'))
+                    screen.close();
+                if (key.getKeyType() == KeyType.EOF)
+                    break;
+
+                arena.moveMonsters();
+                if(arena.verifyMonsterCollisions()){
+                    screen.close();
+                    break;
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private Screen screen;
+    private Arena arena;
 }
